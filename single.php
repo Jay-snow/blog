75e12858ac9c80f3e5dd47ad6db26a125472aaa2
Marcus <Marcusjsnow1@gmail.com>
2018-12-17 07:16:51 -0500

    Added dynamic nav
    Added categories for posts and dynamic navigation. Now there are two categories of posts, "personal" and "work".

---
+++ single.php
@@ -0,0 +1,18 @@
+
+
+
+
+<section>
+
+	<article>
+		<header>
+			<h2> <a> This is a title </a> </h2>
+			<p> <time> 12/10/2018 </time> </p>
+		</header>
+
+		<p> This is the text in the article. </p>
+		
+	</article>
+
+</section>
+
